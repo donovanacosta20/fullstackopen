0f8b848ad2fc460603e54281f64a056a81306bb3
donovanacosta20 <dnvnacosta81@gmail.com>
2022-07-05 11:56:13 -0300

    add middlware

--- part4/blogs-list/app.js
+++ part4/blogs-list/app.js
@@ -2,18 +2,22 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('express-async-errors')
+
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middlware')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const logginRouter = require('./controllers/loggin')
+
 
 const mongoose = require('mongoose')
 
 logger.info('Connecting to MONGODB', process.env.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
 
+app.use(middleware.tokenExtractor)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
--- part4/blogs-list/controllers/blogs.js
+++ part4/blogs-list/controllers/blogs.js
@@ -5,14 +5,6 @@ const Blog = require('../models/blog')
 
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 //GET ALL
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user')
@@ -23,16 +15,14 @@ blogsRouter.get('/', async (request, response) => {
 //POST
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
 
-    const decodedToken = jwt.verify(token, process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-    if (!token || !decodedToken._id) {
+    if (!decodedToken.id) {
         return response.status(401).json({ error: "token missing or invalid" })
     }
 
-
-    const user = await User.findById(body.userId)
+    const user = await User.findById(decodedToken.id)
 
     const newBlog = new Blog({
         title: body.title,
